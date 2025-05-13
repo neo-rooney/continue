647c1620b97ded6906d1f9a7c36ab07a3bca4f93
rooney <bch3454@gmail.com>
2025-05-13 16:01:56 +0900

    feat: 변경 ConfigPage
    - Continue Hub 로그인 버튼 숨김
    - 로그인 폼 추가

--- gui/src/pages/config/index.tsx
+++ gui/src/pages/config/index.tsx
@@ -1,3 +1,13 @@
+/**
+ * ────────────────────────────────────────────────────────────────────────────────
+ * Continue 프로젝트의 ConfigPage 파일을 수정한 버전입니다:
+ * https://github.com/continuedev/continue
+ *
+ * 본 수정은 개발자 배철훈에 의해 2025-05-13에 이루어졌으며, 수정 사항은 다음과 같습니다.
+ * (1) Continue Hub 로그인 버튼 숨김
+ * (2) 로그인 폼 추가
+ * ────────────────────────────────────────────────────────────────────────────────
+ */
 import {
   AcademicCapIcon,
   CircleStackIcon,
@@ -6,15 +16,16 @@ import {
 } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import CustomSesstion from "../../components/custom/CustomSesstion";
+import LoginForm from "../../components/custom/LoginForm";
 import { PageHeader } from "../../components/PageHeader";
+import { useCustomAuth } from "../../context/CustomAuth";
 import { useNavigationListener } from "../../hooks/useNavigationListener";
 import { fontSize } from "../../util";
-import { AccountButton } from "./AccountButton";
 import { HelpCenterSection } from "./HelpCenterSection";
 import { IndexingSettingsSection } from "./IndexingSettingsSection";
 import KeyboardShortcuts from "./KeyboardShortcuts";
 import { UserSettingsForm } from "./UserSettingsForm";
-
 type TabOption = {
   id: string;
   label: string;
@@ -26,6 +37,8 @@ function ConfigPage() {
   useNavigationListener();
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState("settings");
+
+  const { isAuthenticated } = useCustomAuth();
 
   const tabs: TabOption[] = [
     {
@@ -63,9 +76,20 @@ function ConfigPage() {
           showBorder
           onTitleClick={() => navigate("/")}
           title="Chat"
-          rightContent={<AccountButton />}
+          // (1) Continue Hub 로그인 버튼 숨김
+          // rightContent={<AccountButton />}
         />
 
+        {/* (2) 로그인 폼 추가 */}
+        {!isAuthenticated ? (
+          <div className="border-0 border-b-[1px] border-solid border-b-zinc-700 p-4 sm:flex sm:justify-center md:gap-x-2">
+            <LoginForm />
+          </div>
+        ) : (
+          <div className="border-0 border-b-[1px] border-solid border-b-zinc-700 p-4 sm:flex sm:justify-center md:gap-x-2">
+            <CustomSesstion />
+          </div>
+        )}
         {/* Tab Headers */}
         <div className="grid grid-cols-2 border-0 border-b-[1px] border-solid border-b-zinc-700 p-0.5 sm:flex sm:justify-center md:gap-x-2">
           {tabs.map((tab) => (
